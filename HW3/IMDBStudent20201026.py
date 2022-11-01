8aa5808d615121bdfe624b598c52e0bfad732737
kyounggg <dbwlsdbrud1@naver.com>
2022-11-02 06:34:25 +0900

    add HW3 code

--- HW3/IMDBStudent20201026.py
+++ HW3/IMDBStudent20201026.py
@@ -2,3 +2,26 @@
 
 import sys
 
+f = open(sys.argv[1], 'r')
+fw = open(sys.argv[2], 'wt')
+
+dic_genre = {}
+
+for line in f:
+	line = line.strip()
+	temp = line.split('::')
+	genre = temp[2].split('|')
+	for word in genre:
+		if word in dic_genre:
+			dic_genre[word] += 1
+		else:
+			dic_genre[word] = 1
+	
+for k, v in dic_genre.items():
+	fw.write(k)
+	fw.write(" ")
+	fw.write(str(v))
+	fw.write("\n")
+
+f.close()
+fw.close()	
