bdd5b1529b8eca685c87901d96927fd57237c04c
kyounggg <dbwlsdbrud1@naver.com>
2022-12-25 23:20:47 +0900

    add HW4 code

---
+++ HW4/Student20201026.py
@@ -0,0 +1,67 @@
+#!usr/bin/python3
+
+import sys
+from os import listdir
+import numpy as np
+import operator
+
+def createDataSet(dirname):
+    labels = []
+    trainingFileList = listdir(dirname)
+    m = len(trainingFileList)
+    matrix = np.zeros((m, 1024)) 
+
+    for i in range(m): 
+        fileNameStr = trainingFileList[i]
+        answer = int(fileNameStr.split('_')[0])  
+        labels.append(answer)
+        matrix[i, :] = getVector(dirname + '/' + fileNameStr)
+    return matrix, labels 
+
+def classify0(inX, dataSet, labels, k): 
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2 
+    sqDistances = sqDiffMat.sum(axis = 1) 
+    distances = sqDistances ** 0.5 
+    sortedDistIndicies = distances.argsort()
+    classCount = {} 
+
+    for i in range(k): 
+        voteIlabel = labels[sortedDistIndicies[i]]  
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1 
+    sortedClassCount = sorted(classCount.items(), key= operator.itemgetter(1), reverse=True) 
+    return sortedClassCount[0][0]
+
+def getVector(filename):  
+    vector = np.zeros((1, 1024)) # 1024 = 32 x 32
+    with open(filename) as f:
+        for i in range(32):
+            line = f.readline()
+            for j in range(32):
+                vector[0, 32 * i + j] = int(line[j])
+        return vector        
+
+trainingFileDirName = sys.argv[1]
+testFileDirName = sys.argv[2]
+
+testFileList = listdir(testFileDirName)
+length = len(testFileList)
+
+matrix, labels = createDataSet(trainingFileDirName)
+
+for k in range(1, 20, 2): 
+    count = 0 
+    errorCount = 0 
+    
+    for i in range(length): 
+        answer = int(testFileList[i].split('_')[0])
+        testData = getVector(testFileDirName + '/' + testFileList[i])
+        classifiedResult = classify0(testData, matrix, labels, k)
+        
+        count += 1
+        if answer != classifiedResult :
+            errorCount += 1
+    
+    #print(str(errorCount) + "/" + str(count))
+    print(int(errorCount / count * 100))
